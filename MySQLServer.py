3e3edaa452941b065617ff7b41eb4b06012873c0
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-25 02:19:57 +0200

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,4 +1,4 @@
 import mysql.connector
-CREATE DATABASE IF NOT EXITS alx_book_store
+CREATE DATABASE IF NOT EXIST alx_book_store
 db_connection = mysql.connector.connect(host="localhost",user="root",password="SQL.Pass.98")
 print("Connection successful.")
